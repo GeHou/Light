2d9012a6e2afaf820d437af0338d37387d9732d4
LogicHou <logichou@qq.com>
2015-08-06 17:49:16 +0800

    clear test.php file content

--- public/test.php
+++ public/test.php
@@ -1,8 +1,1 @@
 <?php
-$str="UserAddController";
-    /*
-    preg_match_all("/([a-zA-Z]{1}[a-z]*)?[^A-Z]/",$str,$array);
-    */
-    $array=preg_split("/(?=[A-Z])/",$str, -1, PREG_SPLIT_NO_EMPTY);
-    print_r($array);
-    ?>
