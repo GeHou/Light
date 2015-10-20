5294015627310c1eb9de8102be6f2c9409187aff
LogicHou <logichou@qq.com>
2015-10-20 17:46:18 +0800

    u

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 
-define('DS', DIRECTORY_SEPARATOR);
-define('EXT', '.php');
-define('BASE_PATH', __DIR__);
-define('APP_PATH', BASE_PATH . '/app');
-define('SYS_PATH', BASE_PATH . '/system');
-define('VIEW_BASE_PATH', APP_PATH . '/view/');
+// define('DS', DIRECTORY_SEPARATOR);
+// define('EXT', '.php');
+// define('BASE_PATH', __DIR__);
+// define('APP_PATH', BASE_PATH . '/app');
+// define('SYS_PATH', BASE_PATH . '/system');
+// define('VIEW_BASE_PATH', APP_PATH . '/view/');
 
-require 'system/bootstrap.php';
+// require 'system/bootstrap.php';
 
-Light::run();
+// Light::run();
 
-require APP_PATH . '/route.php';
+// require APP_PATH . '/route.php';
+
+$app = require 'system/bootstrap.php';
--- system/bootstrap.php
+++ system/bootstrap.php
@@ -1,42 +1,139 @@
 <?php
 
-require 'core/Loader.php';
-require 'system/tool/Helper.php';
+// require 'core/Loader.php';
+// require 'system/tool/Helper.php';
 
-use system\core\Loader;
-use system\core\Event;
-use system\core\Config;
+// use system\core\Loader;
+// use system\core\Event;
+// use system\core\Config;
 
-Class Light {
+// Class Light {
 
-    protected static $loader;
+//     protected static $loader;
 
-    public static function run()
-    {
-        self::$loader = new Loader;
-        self::$loader->register();
-        self::$loader->addNamespace('system', './system');
-        self::init();
-    }
+//     public static function run()
+//     {
+//         self::$loader = new Loader;
+//         self::$loader->register();
+//         self::$loader->addNamespace('system', './system');
+//         self::init();
+//     }
 
-    public static function init()
-    {
-        Loader::$aliases = array(
-            'Loader' => 'system\\core\\Loader',
-            'Config' => 'system\\core\\Config',
-            'Route' => 'system\\core\\Router',
-            'Controller' => 'system\\core\\Controller',
-            'View' => 'system\\core\\View',
-            'DB' => 'system\\db\\Database',
-            'Event' => 'system\\core\\Event',
-            'L' => 'system\\tool\\Helper',
-        );
-        Event::listen(system\core\Config::loader, function($file)
-        {
-            return system\core\Config::file($file);
-        });
+//     public static function init()
+//     {
+//         Loader::$aliases = array(
+//             'Loader' => 'system\\core\\Loader',
+//             'Config' => 'system\\core\\Config',
+//             'Route' => 'system\\core\\Router',
+//             'Controller' => 'system\\core\\Controller',
+//             'View' => 'system\\core\\View',
+//             'DB' => 'system\\db\\Database',
+//             'Event' => 'system\\core\\Event',
+//             'L' => 'system\\tool\\Helper',
+//         );
+//         Event::listen(system\core\Config::loader, function($file)
+//         {
+//             return system\core\Config::file($file);
+//         });
         
-    }
+//     }
 
-}
+// }
 
+require 'core/Loader.php';
+
+// Dotenv::load(__DIR__.'/../');
+
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| Here we will load the environment and create the application instance
+| that serves as the central piece of this framework. We'll use this
+| application as an "IoC" container and router for this framework.
+|
+*/
+
+$app = new Laravel\Lumen\Application(
+    realpath(__DIR__.'/../')
+);
+
+// $app->withFacades();
+
+// $app->withEloquent();
+
+/*
+|--------------------------------------------------------------------------
+| Register Container Bindings
+|--------------------------------------------------------------------------
+|
+| Now we will register a few bindings in the service container. We will
+| register the exception handler and the console kernel. You may add
+| your own bindings here if you like or you can make another file.
+|
+*/
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+/*
+|--------------------------------------------------------------------------
+| Register Middleware
+|--------------------------------------------------------------------------
+|
+| Next, we will register the middleware with the application. These can
+| be global middleware that run before and after each request into a
+| route or middleware that'll be assigned to some specific routes.
+|
+*/
+
+// $app->middleware([
+//     // Illuminate\Cookie\Middleware\EncryptCookies::class,
+//     // Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+//     // Illuminate\Session\Middleware\StartSession::class,
+//     // Illuminate\View\Middleware\ShareErrorsFromSession::class,
+//     // Laravel\Lumen\Http\Middleware\VerifyCsrfToken::class,
+// ]);
+
+// $app->routeMiddleware([
+
+// ]);
+
+/*
+|--------------------------------------------------------------------------
+| Register Service Providers
+|--------------------------------------------------------------------------
+|
+| Here we will register all of the application's service providers which
+| are used to bind services into the container. Service providers are
+| totally optional, so you are not required to uncomment this line.
+|
+*/
+
+// $app->register(App\Providers\AppServiceProvider::class);
+// $app->register(App\Providers\EventServiceProvider::class);
+
+/*
+|--------------------------------------------------------------------------
+| Load The Application Routes
+|--------------------------------------------------------------------------
+|
+| Next we will include the routes file so that they can all be added to
+| the application. This will provide all of the URLs the application
+| can respond to, as well as the controllers that may handle them.
+|
+*/
+
+$app->group(['namespace' => 'App\Http\Controllers'], function ($app) {
+    require __DIR__.'/../app/Http/routes.php';
+});
+
+return $app;
